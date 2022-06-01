0352085c2c0b55f232ac7a39a1ce6cbd9aabca27
Jordan Hanrahan <jordan.hanrahan@icloud.com>
2022-06-01 11:04:20 +1000

    Added tox from external python project

---
+++ base_finance_data/text_paths.py
@@ -0,0 +1,20 @@
+# This should run once at td startup
+
+import sys
+
+spacer = "- " * 5
+
+for each in sys.path:
+    print(each)
+
+print(spacer)
+
+python_ext = tdu.expandPath(ipar.ExtPython.Target)
+
+if python_ext not in sys.path:
+    sys.path.append(python_ext)
+
+for each in sys.path:
+    print(each)
+
+print(spacer)
