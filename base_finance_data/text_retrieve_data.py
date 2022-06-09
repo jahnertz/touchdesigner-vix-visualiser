6fef123da91b0499b0ae658b9e0bce1db0044eaf
Jordan Hanrahan <jordan.hanrahan@icloud.com>
2022-06-09 15:09:19 +1000

    replaced pdr with yahoo_fin

--- base_finance_data/text_retrieve_data.py
+++ base_finance_data/text_retrieve_data.py
@@ -1,13 +1,6 @@
-import pandas_datareader as pdr
-import datetime as dt
-# import pandas_datareader.data as web
-# import pandas as pd
+from yahoo_fin import stock_info
 
-# data_feed = web.DataReader('^VIX', 'yahoo', start=dt.date.today(), end=dt.date.today())
+vix_current = stock_info.get_live_price("^VIX")
 
-# data_feed = pdr.yahoo.daily.yahoodailyreader(symbols='^vix', start=dt.date.today()).read().close
-vix_data_feed = pdr.data.DataReader('^VIX', 'yahoo', start=dt.date.today())
-vix_close = float(vix_data_feed.Close)
-
-print ('{}'.format(vix_close))
-op('table_vix')[0,0] = vix_close
+print ('{}'.format(vix_current))
+op('table_vix')[0,0] = vix_current
